bf94193dae9e9f5d2401592abdb1357650db6bcc
cash <cash.costello@gmail.com>
2012-02-04 22:14:58 -0500

    1.8 version

--- languages/en.php
+++ languages/en.php
@@ -4,6 +4,7 @@
  */
 
 $english = array(
+	'admin:administer_utilities:adminshout' => 'Email Announcement',
 	'adminshout' => 'Send message to all members',
 	'adminshout:description' => 'Use the form below to send a message to all members of your site. Note that this may take some time.',
 	'adminshout:subject:label' => 'Subject',
--- start.php
+++ start.php
@@ -1,32 +1,13 @@
 <?php
 /**
  * AdminShout - send an email message to all site users
- *
- * @package AdminShout
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @author Curverider Ltd
- * @copyright Curverider Ltd 2008-2010
- * @link http://elgg.org/
-*/ 
+ */
+
+elgg_register_event_handler('init', 'system', 'adminshout_init');
 
 function adminshout_init() {
-	global $CONFIG;
-	register_page_handler('adminshout','adminshout_page_handler');
-	register_action("adminshout/shout",false, $CONFIG->pluginspath . "adminshout/actions/shout.php");
-}
+	elgg_register_admin_menu_item('administer', 'adminshout', 'administer_utilities');
 
-function adminshout_pagesetup() {
-	if (get_context() == 'admin' && isadminloggedin()) {
-		global $CONFIG;
-		add_submenu_item(elgg_echo('adminshout'), $CONFIG->wwwroot . 'pg/adminshout/');
-	}
+	$base = dirname(__FILE__) . '/actions';
+	elgg_register_action('adminshout/send', "$base/send.php", 'admin');
 }
-
-function adminshout_page_handler($page){
-	global $CONFIG;	
-	// only interested in one page for now
-	include($CONFIG->pluginspath . "adminshout/index.php");
-}
-
-register_elgg_event_handler('init','system','adminshout_init');
-register_elgg_event_handler('pagesetup','system','adminshout_pagesetup');
